f1c179477749e540a1e3efbc2dc35a7115fdf800
Dave Gillen <162204811+alexgillen@users.noreply.github.com>
2025-01-29 13:03:22 -0700

    Added a comment for the two classes

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Mission_4_Group_3_13
 {
+    //This file will be the supporting class
     internal class Class1
     {
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-// This is a comment to test how the push commands work
+﻿//Okay this main file is the "Driver" class.
