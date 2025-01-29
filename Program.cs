c08842a5657ac203cd4b3052f2f73e6ecdd28b0a
Dave Gillen <162204811+alexgillen@users.noreply.github.com>
2025-01-29 15:11:32 -0700

    Added full class funciotnality and added breaks in the loop

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+using System.Numerics;
 using System.Runtime.InteropServices;
 using Mission_4_Group_3_13;
 
 Class1 c1 = new Class1();
+bool endGame = false;
 
 Console.WriteLine("Welcome to the game");
 
@@ -11,18 +13,26 @@ for (int i = 0; i < 11; i++)
 {
     if (i < 9)
     {
-        c1.PrintBoard(boardArray[]);
-        
+        endGame = c1.PrintBoard(boardArray);
+        if (endGame)
+        {
+            break;
+        }
         Console.WriteLine("Player 1 please enter a number");
         int number1 = Convert.ToInt16(Console.ReadLine());
         
         boardArray[number1 - 1] = 'X';
-        
-        c1.PrintBoard(boardArray[]);
+
+        endGame = c1.PrintBoard(boardArray);
+        if (endGame)
+        {
+            break;
+        }
         Console.WriteLine("Player 2 please enter a number");
         int number2 = Convert.ToInt16(Console.ReadLine());
-        
+
         boardArray[number2 - 1] = 'O';
+        
     }
     else if (i == 9)
     {
